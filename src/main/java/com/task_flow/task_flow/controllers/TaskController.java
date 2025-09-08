5685ff7d29dddfa9e97a993012b6e7d3c5eb916d
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-09-08 20:31:00 -0300

    injecao de dependencia da interface de serviço

--- src/main/java/com/task_flow/task_flow/controllers/TaskController.java
+++ src/main/java/com/task_flow/task_flow/controllers/TaskController.java
@@ -2,6 +2,7 @@ package com.task_flow.task_flow.controllers;
 
 import com.task_flow.task_flow.dtos.tasks.TaskCreateDTO;
 import com.task_flow.task_flow.dtos.tasks.TaskResponseDTO;
+import com.task_flow.task_flow.services.TaskService;
 import com.task_flow.task_flow.services.impl.TaskScheduling;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +13,45 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskController {
 
-    TaskScheduling taskScheduling;
+    TaskService taskService;
 
-    public TaskController(TaskScheduling taskScheduling) {
-        this.taskScheduling = taskScheduling;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
     @PostMapping
     public ResponseEntity<TaskResponseDTO> createTask(@RequestBody TaskCreateDTO taskCreate) {
-        TaskResponseDTO taskResponse = taskScheduling.createTask(taskCreate);
+        TaskResponseDTO taskResponse = taskService.createTask(taskCreate);
         return ResponseEntity.status(201).body(taskResponse);
     }
 
     @GetMapping
     public ResponseEntity<List<TaskResponseDTO>> getTasks() {
-        List<TaskResponseDTO> tasks = taskScheduling.getTasks();
+        List<TaskResponseDTO> tasks = taskService.getTasks();
         return ResponseEntity.ok(tasks);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> getTaskById(@PathVariable Long id) {
-        TaskResponseDTO taskResponse = taskScheduling.getTaskById(id);
+        TaskResponseDTO taskResponse = taskService.getTaskById(id);
         return ResponseEntity.ok(taskResponse);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable Long id, @RequestBody TaskCreateDTO taskUpdate) {
-        TaskResponseDTO taskResponse = taskScheduling.updateTask(id, taskUpdate);
+        TaskResponseDTO taskResponse = taskService.updateTask(id, taskUpdate);
         return ResponseEntity.ok(taskResponse);
     }
 
     @PatchMapping("/{id}/complete")
     public ResponseEntity<TaskResponseDTO> markTaskAsCompleted(@PathVariable Long id) {
-        TaskResponseDTO taskResponse = taskScheduling.markTaskAsCompleted(id);
+        TaskResponseDTO taskResponse = taskService.markTaskAsCompleted(id);
         return ResponseEntity.ok(taskResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-        taskScheduling.deleteTask(id);
+        taskService.deleteTask(id);
         return ResponseEntity.noContent().build();
     }
 }
